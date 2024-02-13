3020f5f2f0000bfe18c01f18e17e7018e3e76732
WachirayaSrinual <you@example.com>
2024-02-13 10:02:41 +0700

    t

--- AxiosNodeHtml.js
+++ AxiosNodeHtml.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const axios = require('axios');
 const app = express();
+const path = require("path");
 var bodyParser = require('body-parser');
 
 // Base URL for the API
@@ -8,6 +9,7 @@ var bodyParser = require('body-parser');
 const base_url = "http://node58279-wachinoedrest.proen.app.ruk-com.cloud";
 
 // Set the template engine
+app.set("views", path.join(__dirname, "/public/views"))
 app.set('view engine', 'ejs');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
